33a54c657b3b1d958b8766e85bd5243b235ead86
Kristofer Tengström <info@kristofer-tengstrom.se>
2018-05-11 13:33:33 +0200

    kristofer: Added compatibility with Symfony 4

--- Tests/Functional/Services/Decorator/DocumentDecoratorTest.php
+++ Tests/Functional/Services/Decorator/DocumentDecoratorTest.php
@@ -139,7 +139,7 @@ class DoctrineODMDocumentDecoratorTest extends DecoratorBase {
   }
 
   public function testGetAvailableActions() {
-    $laModel = $this->container->get('logauth.service.logauth_model');
+    $laModel = $this->container->get('test.logauth.service.logauth_model');
     $repositoryDecorator = $this->container->get('repository.test_document');
     $documentDecorator = $repositoryDecorator->create();
     $document = $documentDecorator->getDocument();
